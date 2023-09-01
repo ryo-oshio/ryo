34270b422d88de405b5cb1da45d219822f6f68c8
ryo-oshio <syuukatsu0128@gmail.com>
2023-09-01 15:36:26 +0900

    deleteボタン追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,9 +30,22 @@ class PostController extends Controller
         return redirect("/posts/" . $post->id);
     }
     
-    public function show(Post $post)
+    public function edit(Post $post)
     {
-        return view("posts.show")->with(["post" => $post]);
+        return view("oists/edit")->with(["post" => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request["post"];
+        $post->fill($input_post)->save();
+        return redirect("/posts/" . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect("/");
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get("/posts/create",[PostController::class,"create"]);
 Route::get("/posts/{post}",[PostController::class,"show"]);
 Route::post("/posts",[PostController::class,"store"]);
+Route::delete("/posts/{post}",[PostController::class,"delete"]);
